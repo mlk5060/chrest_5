de4060a86f77b67cf73c27f35ea677530aac6cbf
martyn <martynlloydkelly@gmail.com>
2016-03-17 15:44:40 +0000

    Updated main test class to work with new package structure and nulled visual-spatial tests since classes under dev.

--- tests/all-chrest-tests.rb
+++ tests/all-chrest-tests.rb
@@ -9,18 +9,61 @@ include TestFramework
 # Import all required classes
 import "java.util.ArrayList"
 import "java.util.HashMap"
+import "java.awt.Color"
 
 [
   "Chrest", 
+  "Link",
   "Node",
+  "Perceiver",
   "Stm",
   "VisualSpatialField"
 ].each do |klass|
   import "jchrest.architecture.#{klass}"
 end
+
 [
+  "DomainSpecifics",
+  "Fixation",
+  "FixationResult",
+  "Scene",
+  "SceneObject",
+].each do |klass|
+  import "jchrest.domainSpecifics.#{klass}"
+end
+
+[
+  "ChessBoard",
   "ChessDomain",
-  "GenericDomain",
+  "ChessObject"
+].each do |klass|
+  import "jchrest.domainSpecifics.chess.#{klass}"
+end
+
+[
+  "AttackDefenseFixation",
+  "GlobalStrategyFixation",
+  "SalientManFixation"
+].each do |klass|
+  import "jchrest.domainSpecifics.chess.fixations.#{klass}"
+end
+
+[
+  "CentralFixation",
+  "HypothesisDiscriminationFixation",
+  "PeripheralItemFixation",
+  "PeripheralSquareFixation"
+].each do |klass|
+  import "jchrest.domainSpecifics.fixations.#{klass}"
+end
+
+[
+  "GenericDomain"
+].each do |klass|
+  import "jchrest.domainSpecifics.generic.#{klass}"
+end
+
+[
   "ItemSquarePattern",
   "ListPattern",
   "Modality",
@@ -30,18 +73,19 @@ end
   "NumberPattern",
   "Pattern",
   "ReinforcementLearning",
-  "Scene",
-  "SceneObject",
   "Square",
-  "StringPattern",
-  "TileworldDomain"
+  "StringPattern"
 ].each do |klass|
   import "jchrest.lib.#{klass}"
 end
 
 # Pick up all ruby test files except this one
-Dir.glob(File.dirname(__FILE__) + "/*.rb") do |file|
-  require file unless File.expand_path(file) == File.expand_path(__FILE__)
+Dir.glob(File.dirname(__FILE__) + "/**/*.rb") do |file|
+  require file unless 
+  File.expand_path(file) == File.expand_path(__FILE__) || 
+    file.to_s.end_with?("visual-spatial-field-tests.rb") ||
+    file.to_s.end_with?("visual-spatial-field-object-tests.rb") ||
+    file.to_s.end_with?("tileworld-domain-tests.rb")
 end
 
 puts "Testing CHREST:"
